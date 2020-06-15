50ac422b133de6ffdeb4985320754eb4b7639de6
Valentin Berger <Valentin.berger38@gmail.com>
2020-06-15 18:15:05 +0200

    :green_heart: :negative_squared_cross_mark: Commit an error to test the CI

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: Theme.of(context).textTheme.headline4,
               ),
               SearchPage()
-            ],
+            ]
           ),
         ),
       ),
